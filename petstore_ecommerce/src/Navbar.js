136e4cfba45397279b1c1863443627075094c1dd
dplascenciap <dplascenciap@gmail.com>
2023-03-20 22:19:35 -0400

    Add list of items into the Navbar

--- petstore_ecommerce/src/Navbar.js
+++ petstore_ecommerce/src/Navbar.js
@@ -2,7 +2,17 @@
 function Navbar() {
     return (
         <div className="container">
-            <p>This will be the navbar</p>
+            <nav>
+                <p>This will be the navbar</p>
+                <ul className="nav-items">
+                    <li><a href="">shop</a></li>
+                    <li><a href="">new items</a></li>
+                    <li><a href="">special offers</a></li>
+                    <li><a href="">track your order</a></li>
+                    <li><a href="">FAQs</a></li>
+                    <li><a href="">register/login</a></li>
+                </ul>
+            </nav>
         </div>
 );
 }
