ae2fef5b7e569f793fa2fdf383515edede4927d3
dplascenciap <dplascenciap@gmail.com>
2023-03-29 22:34:38 -0400

    Removing extra components from App.js and index.js

--- petstore_ecommerce/src/index.js
+++ petstore_ecommerce/src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Navbar from './Navbar'
+//import Navbar from './Navbar'
 import './styles.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <body>
-      <div className='container'>
-        <Navbar/>
-      </div>
-    </body>
+    <App />
   </React.StrictMode>
 );
 
