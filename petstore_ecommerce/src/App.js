d86f1c0d593a926279b3cbaddcb181e9bcb952cf
dplascenciap <dplascenciap@gmail.com>
2023-03-31 20:29:25 -0400

    Reorganize components

--- petstore_ecommerce/src/App.js
+++ petstore_ecommerce/src/App.js
@@ -1,14 +1,14 @@
 //import logo from './logo.svg';
 //import './App.css';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar/>
       </header>
       <body>
+        <Navbar/>
       </body>
     </div>
   );
