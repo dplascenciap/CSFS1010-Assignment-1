797aad8c8da4148f81790e5227f74a7cf567e3f1
dplascenciap <dplascenciap@gmail.com>
2023-04-12 17:02:59 -0400

    Add style to copyright section

--- petstore_ecommerce/src/components/Copyright.js
+++ petstore_ecommerce/src/components/Copyright.js
@@ -2,17 +2,20 @@
  * Function Copyright()
  * @returns Navigation Copyright component
  */
- function Copyright() {
+function Copyright() {
     return (
         <div className="copyright">
-            <p>Copyright &copy; 2022, Pet Store</p>
-            <img src="./assets/payment_cards.png"
-                alt="Logo of different payment cards"
-                id="img_cards"
-                width={128}
-            />
+            <div>
+                <span>Copyright &copy; 2022, Pet Store</span>
+            </div>
+            <div>
+                <img src="./assets/payment_cards.png"
+                    alt="Logo of different payment cards"
+                    id="img_cards"
+                    width={128}
+                />
+            </div>
         </div>
     );
 }
-
 export default Copyright;
