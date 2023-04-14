d1ad3872eed6873c0632f6180b6322ce030da7e4
dplascenciap <dplascenciap@gmail.com>
2023-04-13 22:51:01 -0400

    Add products to the Shop page

--- petstore_ecommerce/src/components/ShopPage.js
+++ petstore_ecommerce/src/components/ShopPage.js
@@ -1,15 +1,26 @@
+import products from "./data/products.json"
+
 /**
  * Function ShopPage()
  * @returns Shop page component
  */
-
  function ShopPage() {
     return (
         <div>
             <h1>Shop</h1>
-            <p>
-                This is Shop page
-            </p>
+            <div className="search-bar">
+                <h2>Search Products</h2><br></br>
+                <input type="search" id="search"/>
+            </div>
+            <div className="productList">
+                {products.map((productItem) => (
+                    <div className="product">
+                        <li>{productItem.name}</li>
+                        <li>{productItem.supplier}</li>
+                        <li>{productItem.price}</li>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
