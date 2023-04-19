f0b73c59ab4c5f1563d13f650807ef9f1ec29970
dplascenciap <dplascenciap@gmail.com>
2023-04-18 21:36:58 -0400

    Make search bar stateless

--- petstore_ecommerce/src/components/SearchBar.js
+++ petstore_ecommerce/src/components/SearchBar.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
 /**
  * Function SearchBar()
  * @returns SearchBar page component
+ * Original stated function.
  */
-function SearchBar(props){
+/*
+import { useState } from "react";
+function SearchBar_original(props){
     const [searchText, setSearchText] = useState("")
     return (
         <div className="search-bar">
@@ -18,5 +20,24 @@ function SearchBar(props){
         </div>
     )
 }
+*/
+/**
+ * Function SearchBar()
+ * @returns SearchBar page component
+ * New stateless function.
+ */
+function SearchBar(props){
+    return (
+        <div className="search-bar">
+            <h2>Search Products</h2>
+            <input type="search"
+                onChange={(e) => {
+                    props.callBack(e.target.value)
+                }}
+            />
+        </div>
+    )
+}
+
 
 export default SearchBar;
